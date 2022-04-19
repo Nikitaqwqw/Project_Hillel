2e14ca35968bc6cb3b9ee08cf0445eaf1b49df6f
Nikita <nksenofontov97@gmail.com>
2022-04-19 20:40:05 +0300

    our third commit new branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         int i = 0;
+
+    }
+
+    static void print(){
+        System.out.println("Branch -> new branch");
     }
 
 }
